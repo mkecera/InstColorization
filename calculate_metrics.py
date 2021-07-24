30a6b9c46443cefb2ed8be09501b8101152a8d96
qian <anqi970301@outlook.com>
2021-07-24 20:16:26 +0200

    updated validation, base_model, fusion_model

---
+++ calculate_metrics.py
@@ -0,0 +1,70 @@
+import os
+from os.path import join
+import time
+from options.train_options import TrainOptions, TestOptions
+from models import create_model
+from util.visualizer import Visualizer
+
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from tqdm import trange, tqdm
+
+from fusion_dataset import Fusion_Testing_Dataset
+from util import util
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+import numpy as np
+import multiprocessing
+multiprocessing.set_start_method('spawn', True)
+
+torch.backends.cudnn.benchmark = True
+
+import cv2
+import skimage
+
+if __name__ == '__main__':
+    opt = TestOptions().parse()
+    save_img_path = opt.results_img_dir
+    if os.path.isdir(save_img_path) is False:
+        print('Create path: {0}'.format(save_img_path))
+        os.makedirs(save_img_path)
+    opt.batch_size = 1
+    dataset = Fusion_Testing_Dataset(opt)
+    dataset_loader = torch.utils.data.DataLoader(dataset, batch_size=1, num_workers=2)
+
+    dataset_size = len(dataset)
+    print('#Testing images = %d' % dataset_size)
+
+    # model = create_model(opt)
+    # model.setup_to_test('coco_finetuned_mask_256')
+    # model.setup_to_test('coco_tiny_fusion')
+
+    count_empty = 0
+    all_psnr = np.array([])
+    all_ssim = np.array([])
+    for data_raw in tqdm(dataset_loader, dynamic_ncols=True):
+        
+        # calculate metrics
+        # get true image
+        true_img = cv2.imread(join('./', opt.test_img_dir, data_raw['file_id'][0] + '.jpg'))
+        # resize true image
+        true_img = cv2.resize(true_img, (256, 256), interpolation=cv2.INTER_AREA)
+
+        # get test image
+        test_img = cv2.imread(join('./', opt.results_img_dir, data_raw['file_id'][0] + '.png'))
+
+        # calculate psnr
+        psnr = skimage.metrics.peak_signal_noise_ratio(true_img, test_img)
+
+        # calculate ssim
+        ssim = skimage.metrics.structural_similarity(true_img, test_img, multichannel=True)
+
+        # save to array
+        all_psnr = np.append(all_psnr, psnr)
+        all_ssim = np.append(all_ssim, ssim)
+
+    print('{0} PSNR'.format(np.average(all_psnr)))
+    print('{0} SSIM'.format(np.average(all_ssim)))
+    print(all_psnr)
+    print(all_ssim)
