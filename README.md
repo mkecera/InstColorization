14b5d7084912014a668b81a704619529dc63ccb1
gomni <khalil.nouili@gmail.com>
2021-08-02 23:07:08 +0200

    update readme file

--- README.md
+++ README.md
@@ -7,7 +7,6 @@ In order to setup a local environment, create a venv and run `pip install -r req
 ## How to run on Google Colab
 
 The results can be reproduced by running the notebooks placed under `/notebooks`.
-
 The notebooks can be opened in `Colab` and self-contain the environment installation
 
 ### Notebooks
@@ -24,3 +23,32 @@ The notebooks can be opened in `Colab` and self-contain the environment installa
 - `inference_bbox.py` Creates bounding boxes of a dataset
 - `calculate_metrics.py` Calculates metrics for a test image set
 - `/models` Includes the different models of our implementation
+
+## ConvLSTM extension
+
+### Training
+1. Put the training data (all of your videos) under video_module/data/training.  
+2. Run this command in order to extract the frames from your videos and build the training files structure:
+```
+python3 video_modules/data_preprocessing.py
+```
+3. Run this command in order to extract the inference bounding boxes from all the frames.
+```
+python3 video_modules/inference_bbox.py
+```
+4. Run this command in order to get the output for all the frames from instance colorization approach.
+```
+python3 test_fusion_for_video_module.py
+```
+5. Run this command to train the ConvLSTM model.
+```
+python3 lstm_main.py --train
+```
+
+### Testing
+1. You need to [download](https://drive.google.com/drive/folders/1r3PoTE9K0iX-7K56xt8AI6SKlBBANQ-c?usp=sharing) the model checkpoint and to put in under video_modules/models/
+2. Repeat training steps from 1 to 4 under training section while replacing the training data with the testing one.
+3. Run this command to get the predictions
+```
+python3 lstm_main.py
+```
--- lstm_main.py
+++ lstm_main.py
@@ -1,17 +1,24 @@
 import sys
 from video_module.data_loader import DataSetLoader
-
+import cv2
 import matplotlib.pyplot as plt
 import torch
 from torch.utils.data import DataLoader
 import pytorch_lightning as pl
 from pytorch_lightning import Trainer
-
+import time
 torch.cuda.empty_cache()
 
 sys.path.append("home/gomni/Documents/InstColorization/")
 
 from video_module.models.lstm_model.encoder_decoder import ArchitectureConvLSTM
+import argparse
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--train', action='store_true',)
+opt = parser.parse_args()
+
+args, leftovers = parser.parse_known_args()
 
 
 class ConvLSTMMain(pl.LightningModule):
@@ -39,13 +46,15 @@ class ConvLSTMMain(pl.LightningModule):
         output = self.model(x, future_seq=self.n_steps_ahead)
         return output
 
-    def training_step(self, batch, batch_idx):
+    def save_picture(self, image):
+        file_name = './video_module/models/lstm_model/results/' + str(time.strftime("%Y%m%d-%H%M%S")) + '.png'
+        cv2.imwrite(file_name, image)
 
+    def training_step(self, batch, batch_idx):
         x, y = batch[:, 0, 0:self.n_steps_past, :, :, :], batch[:, 1, self.n_steps_past, :, :, :].unsqueeze(1)
         x = x.permute(0, 1, 4, 2, 3)
         y = y.permute(0, 1, 4, 2, 3)
         y = y.squeeze()
-
         y_hat = self.forward(x).squeeze()
         loss = self.criterion(y_hat, y)
 
@@ -54,6 +63,7 @@ class ConvLSTMMain(pl.LightningModule):
     def test_step(self, batch, batch_idx):
         x, y = batch
         y_hat = self.forward(x)
+        self.save_picture(y_hat)
         return {'test_loss': self.criterion(y_hat, y)}
 
     def configure_optimizers(self):
@@ -65,9 +75,9 @@ class ConvLSTMMain(pl.LightningModule):
         return train_loader
 
     def test_dataloader(self):
-        a = DataSetLoader(data_root='./video_module/data/testing')
-        train_loader = torch.utils.data.DataLoader(dataset=a, batch_size=self.batch_size, shuffle=False)
-        return train_loader
+        a = DataSetLoader()
+        test_loader = torch.utils.data.DataLoader(dataset=a, batch_size=self.batch_size, shuffle=False)
+        return test_loader
 
 
 def run_trainer(n_steps_ahead=1, epochs=200, in_channel=3):
@@ -77,13 +87,15 @@ def run_trainer(n_steps_ahead=1, epochs=200, in_channel=3):
     trainer.fit(model)
 
 
-def run_trainer(n_steps_ahead=1, epochs=200, in_channel=3):
+def run_tester(n_steps_ahead=1, epochs=200, in_channel=3):
     conv_lstm_model = ArchitectureConvLSTM(nf=n_steps_ahead, in_chan=in_channel)
     model = ConvLSTMMain(model=conv_lstm_model)
-    trainer = Trainer(max_epochs=epochs)
-    output = trainer.evaluate(model)
+    trainer = Trainer()
+    trainer.test(model)
 
 
 if __name__ == '__main__':
-    run_trainer()
-    # run_tester()
+    if args.train is not None:
+        run_trainer()
+    else:
+        run_tester()
